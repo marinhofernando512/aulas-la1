fdd91c2ae58c7b1a2887c5b88d06d1b5c32c42fa
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-03-26 18:03:37 -0300

    Add files via upload

---
+++ vetorexercicio01.py
@@ -0,0 +1,13 @@
+#1) Leia um vetor de 10 posições. Calcule e escreva quantos valores pares ele possui
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for numero in numeros:   
+    print(numero)
+    if numero % 2 == 0:
+        print(f" o número {numero} é par")
+    print(numero)
+
+
+
+
+
