1b567672976c9e2dee3ebb039e8399cdb86d663d
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-03-26 16:05:32 -0300

    Add files via upload

---
+++ exercicio02Aula04.py
@@ -0,0 +1,15 @@
+# Elabore três scripts que mostrem a mensagem "Estou aparecendo pela <numero_de_vezes> na tela!" 10 vezes.
+#  Substitua <numero_de_vezes> pela iteração atual do loop. 
+# Utilize uma estrutura de repetição diferente em cada um dos scripts (for e while).
+
+
+
+
+for i in range(10):
+    print(f" Estou aparecendo pelo Mundo 1!")
+    print(f" Estou aparecendo pelo Mundo 2!")
+    print(f" Estou aparecendo pelo Mundo 3!")
+    print(f" FIM!")
+
+
+
