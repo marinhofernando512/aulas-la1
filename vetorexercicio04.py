ac392a505a288d6ab52bf8d868da16d159ed313c
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-03-26 18:16:56 -0300

    Add files via upload

---
+++ vetorexercicio04.py
@@ -0,0 +1,13 @@
+#4) Ler um vetor de 5 elementos. Crie um segundo vetor, com todos os elementos na ordem 
+#inversa, ou seja, o último elemento passará a ser o primeiro, o penúltimo será o segundo 
+#e assim por diante. Mostre os dois vetores. 
+
+numeros = [1, 2, 3, 4, 5]
+for numero in numeros[::-1]:
+    print(numero)
+
+    
+
+
+
+
