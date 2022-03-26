bd38a7bf393e7f14276beb034c8ba3e25e230578
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-03-26 16:21:18 -0300

    Add files via upload

---
+++ VetoresAula05.py
@@ -0,0 +1,39 @@
+vetor = ["a", 2, True, 2.1]
+print(len(vetor))
+
+# tamanho do vetor - len
+
+print(vetor[3])
+print(vetor[0])
+print(vetor[1:2])
+print(vetor[::-1]) # iltimo elemento do vetor
+print(vetor[-2])
+
+#Adicionar elementos
+#outroVetor[0] = input ("Digite um valor")
+#print(outroVetor)
+
+#outroVetor.append(5) # add final 
+#print(outoVetor)
+
+#outroVetor.insert(1, 2) # add no inicio
+#print(outroVetor)
+
+#outroVetor.insert(0, 10)
+
+outroVetor = [1, 2, 3]
+outroVetor.insert(1, 10)
+
+numeros = [1, 2, 3, 4, 5]
+for numero in numeros:
+    print(numero)
+    if numero % 2 == 0:
+        print(f" o número {numero} é par")
+
+
+
+
+
+
+
+
