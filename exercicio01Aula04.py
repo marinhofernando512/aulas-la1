d9d5173d3ee1fe8ead2fdd3fddddb21ff1a5531e
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-03-26 16:04:48 -0300

    Add files via upload

---
+++ exercicio01Aula04.py
@@ -0,0 +1,9 @@
+#  Faça dois scripts que mostrem a mensagem "Olá mundo!" 10 vezes na tela.
+#  Utilize uma estrutura de repetição diferente em cada um dos scripts (for e while).
+
+for i in range(10):
+    print(f" Ola Mundo!")
+
+
+
+
