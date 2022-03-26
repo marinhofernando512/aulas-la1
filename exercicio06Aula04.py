0d86c48e3ca47f93560d5b5353e5c8e6a927bc63
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-03-26 17:22:59 -0300

    Add files via upload

---
+++ exercicio06Aula04.py
@@ -0,0 +1,29 @@
+
+
+
+soma = 0
+for i in ranger(5)
+    valor= int(input("Digite o Valor"))
+    soma = soma + valor 
+    soma += valor
+
+print(f"Soma: {soma}")
+media = soma / 5
+print (f"Média:.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
