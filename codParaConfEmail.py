8484dcfac24bff7f2c468e232a1837e7a244378f
FERNANDO MARINHO <101125621+marinhofernando512@users.noreply.github.com>
2022-04-19 18:58:43 -0300

    Add files via upload

---
+++ codParaConfEmail.py
@@ -0,0 +1,20 @@
+import re
+
+regex = re.compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+        print("Valid email")
+    else:
+        print("Invalid email")
+
+isValid("")
+
+    
+
+
+
+
+ 
+      
+
